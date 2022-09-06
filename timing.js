950c13d5fe1a35abe04e04126937f69c99725526
Ryan Lindner <ryan.m.lindner@gmail.com>
2022-09-06 16:39:01 -0600

    work for 9/6, splitting JS file and beginning to understand setInterval

--- timing.js
+++ timing.js
@@ -1,5 +1,9 @@
-<!-- Templating? External counting script V0 -->
-setInterval(resourceCounting(getElementById("chara")), 1000);
+
+//how deep to template?
+//how to call the value of 'chara' and then use a more complex data type to connect everything?
+//TODO object definitions in HTML vs. JS
+
+setInterval(resourceCounting(document.getElementById("chara")), 1000);
 function resourceCounting(resource) {
   counter = resource.innerHTML;
   counterGrowth = resource.growth;
