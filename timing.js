5fe53e5ee2f561b0c69aa5e1ff05a02946607b45
Ryan Lindner <ryan.m.lindner@gmail.com>
2022-09-07 16:49:03 -0600

    9/7, some redefinition of variables in JS to bigint Not a working solution, but understanding will happen eventually

--- timing.js
+++ timing.js
@@ -3,9 +3,14 @@
 //how to call the value of 'chara' and then use a more complex data type to connect everything?
 //TODO object definitions in HTML vs. JS
 
-setInterval(resourceCounting(document.getElementById("chara")), 1000);
-function resourceCounting(resource) {
+//NOT IN USE YET, NO DEBUG
+let chara = 0n;
+let charaGrowth = 0n;
+setInterval(resourceCounting(document.getElementById("charaTotal"), chara, charaGrowth), 1000);
+function resourceCounting(resource, resourceType, resourceGrowth) {
+  counter = 0n;
+  counterGrowth = 0n;
   counter = resource.innerHTML;
-  counterGrowth = resource.growth;
+  counterGrowth = resourceGrowth;
   counter += counterGrowth;
 }
