02f81afd2b47f809659a13da39880c725539ee41
Ryan Lindner <ryan.m.lindner@gmail.com>
2022-09-16 22:32:55 -0600

    script moved into JS file, generator class working

--- timing.js
+++ timing.js
@@ -4,7 +4,6 @@
 //TODO object definitions in HTML vs. JS
 
 //creating and linking list of resources (linked list lol jk), refer to map in notebook, UPDATE HERE FIRST:: USE ID's IN HTML
-//TODO get plugins set up for HTML objects -> JS objects
 /*
 plugins for to get T1 working:
 chara
@@ -13,24 +12,22 @@ autoclickers
 macros
 monitors
 summons
+plugins plugged in, working ish **need to test GEN 2-5 and connect buttons, only keyboards is connected up
 */
-
-let chara = 100n;
 
 //class for generators to keep track of cost, costgrowth, and change in chara growth
 class generatorChara {
-  constructor(basecost, costgrowth, tier, currency, growthFactor) {
+  constructor(basecost, costgrowth, tier, button, growthFactor) {
   this.basecost = basecost;
   this.costgrowth = costgrowth;
   this.tier = tier;
-  this.currency = chara;
+  this.button = button;
   this.growthFactor = growthFactor;
-  this.realGrowth = 0n;
   this.amount = 0;
   }
   realcost() {
     //TODO this is right?
-    return (this.basecost*(Math.pow(this.costgrowth, amount)))
+    return Math.floor((this.basecost*(Math.pow(this.costgrowth, this.amount))))
   }
   buyOne() {
     let bought = false;
@@ -56,20 +53,17 @@ class generatorChara {
     }
     return bought;    
   }
-  updateGrowth() {
-    this.realGrowth = this.growthFactor*this.amount;
-  }
 }
 
 //TODO class definitions done, rewrite variables as generators when appropriate
 // prestige layer 1 gen
 
 //BALANCEPOINT
-let keyboards = new generatorChara(100n, 1.21, 1, chara, 1n);
-let autoclickers = new generatorChara(1000n, 1.31, 2, chara, 10n);
-let macros = new generatorChara(10000n, 1.41, 3, chara, 100n);
-let monitors = new generatorChara(100000n, 1.51, 4, chara, 1000n);
-let summons = new generatorChara(1000000n, 1.61, 5, chara, 10000n);
+let $keyboards = new generatorChara(100, 1.21, 1, document.getElementById("gen1"), 1n);
+let $autoclickers = new generatorChara(1000n, 1.31, 2, document.getElementById("gen2"), 10n);
+let $macros = new generatorChara(10000n, 1.41, 3, document.getElementById("gen3"), 100n);
+let $monitors = new generatorChara(100000n, 1.51, 4, document.getElementById("gen4"), 1000n);
+let $summons = new generatorChara(1000000n, 1.61, 5, document.getElementById("gen5"), 10000n);
 
 let ingenuity = 0n;
 // prestige layer 2 gen
@@ -88,24 +82,31 @@ let graphs = 0n;
 let codeCleanliness = 0n;
 
 
-//create custom class to handle connections between raw amounts and things like cost increase, base cost, real cost, etc.?
-/*
-class ...
-*/
+//use explicit type definitions to enforce addition instead of concatenation due to JS automatic typing
+let $chara = BigInt(document.getElementById("charaTotal").innerHTML);
+let $charaGrowth = 0n;
 
+$keyboards.button.addEventListener("click", buyOneGenerator);
+
+function buyOneGenerator() {
+  if ($chara >= $keyboards.realcost()) {
+  $charaGrowth = $charaGrowth + $keyboards.growthFactor; //balancepoint for testing
+  $chara = $chara - BigInt($keyboards.realcost());
+  $keyboards.amount++;
+  document.getElementById("keyboards").innerHTML = $keyboards.amount;
+  document.getElementById("keyboardsCost").innerHTML = $keyboards.realcost();
+
+  }
+}
+
+setInterval(charaGrow, 100);
+function charaGrow(){
+  $chara = $chara + $charaGrowth;
+  document.getElementById("charaTotal").innerHTML = $chara;
+}
 //NOT IN USE YET, NO DEBUG
 
 //3 prestige tiers & growth factors
 let charaGrowth = 0n;
 let ideasGrowth = 0n;
 let packagingGrowth = 0n;
-
-// TODO refactor setinterval to take ANY resource as args and output the same
-setInterval(resourceCounting(document.getElementById("charaTotal"), chara, charaGrowth), 1000);
-function resourceCounting(resource, resourceType, resourceGrowth) {
-  counter = 0n;
-  counterGrowth = 0n;
-  counter = resource.innerHTML;
-  counterGrowth = resourceGrowth;
-  counter += counterGrowth;
-}
