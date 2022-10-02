f38cf4eda9565299e84af6d488acfb5f58cfd872
Ryan Lindner <ryan.m.lindner@gmail.com>
2022-10-02 17:51:42 -0600

    start of api foundation. empty file to get started on /something/

---
+++ saveData.py
@@ -0,0 +1,18 @@
+# API necessities
+# save database (csv file? TODO figure out how to host a save database? external? idk)
+# database consists of name : JSON save string? how to update saves? automatic?
+# classic import/export functionality TBD
+# action : http method
+# save creation POST
+# save deletion DELETE
+# save update PUT
+# restful constraints for reference
+# uniform interface
+# client // server
+# stateless
+# cacheable
+# layered system
+# code on demand (opt) (not doing)
+# build API using FLASK
+# connect to something we'll figure it out
+
