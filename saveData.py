c27dc6b676133b57bac2839c9725beb7ea972011
Ryan Lindner <ryan.m.lindner@gmail.com>
2022-10-06 17:24:25 -0600

    Revert "setup for connection to MongoDB"
    This reverts commit 4eb1e7da9876bd3e5268d70b6fd46c39c7aa21d9.

--- saveData.py
+++ saveData.py
@@ -1,7 +1,7 @@
 # API necessities
 # save database (csv file? TODO figure out how to host a save database? external? idk)
-# database consists of collections, mirror the object interface in JS code,
-# 15 objects in code, store instances of each as a save file?
+# database consists of name : JSON save string? how to update saves? automatic?
+# classic import/export functionality TBD
 # action : http method
 # save creation POST
 # save deletion DELETE
@@ -14,26 +14,25 @@
 # layered system
 # code on demand (opt) (not doing)
 # build API using FLASK
-# database using MONGODB
-# connecting using pymongo
-# update/interact with flask
+# connect to something we'll figure it out
 
 import datetime
 import os
 
+from inspect import _void
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-# load config from a .env file:
+# Load config from a .env file:
 load_dotenv()
 MONGODB_URI = os.environ['MONGODB_URI']
 
-# connect to cluster:
+# Connect to your MongoDB cluster:
 client = MongoClient(MONGODB_URI)
 
-# list all the databases in the cluster:
+# List all the databases in the cluster:
 for db_info in client.list_database_names():
    print(db_info)
 
