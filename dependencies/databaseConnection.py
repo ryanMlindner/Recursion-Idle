2b6403d0729f84c613749cf7a21099b4d071130e
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-02-02 07:01:03 -0700

    reformat without secrets for main

--- dependencies/databaseConnection.py
+++ dependencies/databaseConnection.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
-connectString = {{___DB_CONNECT_STRING___}}
-MONGODB_URI = os.environ[connectString]
+#connectString = {{___DB_CONNECT_STRING___}}
+MONGODB_URI = os.environ['MONGODB_URI']
 # Connect to MongoDB cluster:
 client = MongoClient(MONGODB_URI)
 #database connection
